2601ab0f791e304e467dc1b530aefcdadd42e86a
aspa3405 <aspa3405@colorado.edu>
2023-02-15 16:37:04 -0700

    cf2

--- CF2/Week5/whydidthecowcrosstheroad-apaulson.py
+++ CF2/Week5/whydidthecowcrosstheroad-apaulson.py
@@ -1,16 +1,29 @@
 with open('cowqueue.in') as fin:
-  n = fin.readline()
+  N = fin.readline()
   cows = []
   for line in fin.readlines():
     cows.append(list(map(int, line.split())))
   
-print(n)
-print(cows)
+#print(N)
+#print(cows)
 
 cur_time = 0
 
+cows.sort()
+#print(cows)
 
-
+for i in range(len(cows)):
+  #print(cows[i][start])
+  #print(cows[i][duration])
+  sequence = []  
+  if (cows[0][0] + cows[0][1]) < cows[1][0]:
+    #print(cows[1][0])
+    sequence.append(cows[1][0]+cows[1][1])
+    #print(sequence)
+    if(str(sequence))>(str(cows[2][0]+cows[2][1])):
+      sequence.append(str(cows[2][1]))
+      #print(sequence)
+  cur_time = int(sequence[0]) + int(sequence[1])
 #cur_time should be the final output the problem expects
 print(cur_time)
 print(cur_time, file=open('cowqueue.out', 'w'))
